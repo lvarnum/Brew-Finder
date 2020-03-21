59fa49a0654517d161e7dc56dfaebcd3a0286d2e
Ryan Stewart <rjstewart@email.arizona.edu>
2020-03-21 13:37:06 -0700

    ajax calls

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 $("#cocktails").on("click", function (event) {
     event.preventDefault();
-    window.location.href="search.html";
-    
+    window.location.href = "search.html";
+
 })
 
 $("#meals").on("click", function (event) {
     event.preventDefault();
-    window.location.href="search.html";
-    
+    window.location.href = "search.html";
+
 })
 
-$(".pure-button").on("click", function (event){
+$(".pure-button").on("click", function (event) {
     event.preventDefault();
     window.location.href = "search.html";
 })
+
+
+var term = $("#Search");
+term = term.val();
+console.log(term);
+
+var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + term;
+// Performing an AJAX request with the queryURL
+$.ajax({
+    url: queryURL,
+    method: "GET"
+})
+    // After data comes back from the request
+    .then(function (response) {
+        console.log(queryURL);
+
+        console.log(response);
+    });
